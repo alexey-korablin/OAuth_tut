afc949b43e71acb57941de9439bd31c455c11653
Alexey Korablin <Alexey_Korablin@epam.com>
2020-03-13 17:07:14 +0300

    #03 Implements basic authorization with Google plus API

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
+const keys = require('./keys');
+
 // register strategy middleware
-passport.use(new GoogleStrategy({}), () => {});
+passport.use(
+  new GoogleStrategy(
+    {
+      // Options for the google strategy
+      callbackURL: 'auth/google/redirect',
+      clientID: keys.google.clientId,
+      clientSecret: keys.google.clientSecret,
+    },
+    () => {
+      // passport callback function
+    },
+  ),
+);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+
+// empty comment
 
 const app = express();
 const PORT = 5000;
