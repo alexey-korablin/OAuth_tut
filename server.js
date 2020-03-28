647d4925df32131a04c50f0bcf06a77e60f571de
Alexey Korablin <alex8456@bk.ru>
2020-03-29 00:07:25 +0300

    #09 Stes up cookie session by using middlewares

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const db = require('./db');
+const keys = require('./config/keys');
 
-// empty comment
-
+// working on db (mongodb) events
 db.on('open', () => {
   console.log('=> connected to mongodb');
 });
@@ -15,9 +17,25 @@ db.on(
   console.error.bind(console, 'MongoDB connection error: '),
 );
 
+//  initialize node express application
 const app = express();
 const PORT = 5000;
 
+// set up middlewares
+// set up cookieSession middleware. The cookie will be actual for one
+// day (86400000 msec = 24*60*60*1000) and it will be enc/dec by
+// passed cookieKey
+app.use(
+  cookieSession({
+    maxAge: 86400000,
+    keys: keys.session.cookieKey,
+  }),
+);
+//  initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//  set up view engine
 app.set('view engine', 'ejs');
 
 // set up routes
@@ -27,4 +45,5 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+// starting the app on certain port of localhost
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
